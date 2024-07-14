9429dde07c52d94bb1c9c2d6ca19a6666c25b635
Sergio De Luca <serg.r.deluca@gmail.com>
2024-07-14 18:07:57 -0300

    many to many

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
@@ -41,4 +43,19 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function ideas():HasMany
+    {
+        return $this->hasMany(Idea::class);
+    }
+
+    public function ideasLiked():BelongsToMany
+    {
+        return $this->belongsToMany(Idea::class);
+    }
+
+
+
+
+
 }
--- database/migrations/2024_07_14_205110_create_idea_user_table.php
+++ database/migrations/2024_07_14_205110_create_idea_user_table.php
@@ -13,12 +13,10 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('ideas', function (Blueprint $table) {
+        Schema::create('idea_user', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('idea_id')->constrained()->cascadeOnDelete();
             $table->foreignId('user_id')->constrained()->cascadeOnDelete();
-            $table->string('title');
-            $table->text('description');
-            $table->unsignedInteger('likes');
             $table->timestamps();
         });
     }
@@ -30,6 +28,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('ideas');
+        Schema::dropIfExists('idea_user');
     }
 };
